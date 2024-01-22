5d8f54d4bcf5c891d3bd281ef5cf2da6a975519f
Ryan D Gregory <ryang872@gmail.com>
2024-01-21 18:01:28 -0600

    settings.py

--- abqgh/settings.py
+++ abqgh/settings.py
@@ -176,9 +176,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
